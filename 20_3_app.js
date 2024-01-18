313c0bfcecc24fb7cd906ad3385a7afbe42b3ab6
yasuhiro.m <yasuhiro@m.com>
2024-01-17 23:25:22 -0800

    adding 20_3

---
+++ 20_3_app.js
@@ -0,0 +1,33 @@
+const $gifArea = $("#gif-area");
+const $searchInput = $("#search");
+
+function addGif(res) {
+  // console.log(res);
+  let numResults = res.data.length;
+  if (numResults) {
+    const randomIdx = Math.floor(Math.random() * numResults);
+    const $newGif = $("<img>", {
+      src: res.data[randomIdx].images.original.url
+    });
+    $gifArea.append($newGif);
+  }
+}
+
+$("form").on("submit", async function(evt) {
+  evt.preventDefault();
+
+  let searchTerm = $searchInput.val();
+  $searchInput.val("");
+
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: searchTerm,
+      api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+    }
+  });
+  addGif(response.data);
+});
+
+$('#remove').on("click", function() {
+  $gifArea.empty();
+})
